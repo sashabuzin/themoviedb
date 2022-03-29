530187247f23851c3a525315eadef4f0b5764539
sashabuzin <n9159648714@gmail.com>
2022-03-29 21:59:50 +0300

    Create ActorLoader object and create loading actorData from server

--- app/src/main/java/com/buzinasgeekbrains/themoviedb/model/ActorDTO.kt
+++ app/src/main/java/com/buzinasgeekbrains/themoviedb/model/ActorDTO.kt
@@ -1,5 +1,19 @@
 package com.buzinasgeekbrains.themoviedb.model
 
-//data class ActorDTO(
-//
-//)
+import com.google.gson.annotations.SerializedName
+
+data class ActorDTO(
+    val also_known_as: Array<String>,
+    val biography: String?,
+    val birthday: String?,
+    val deathday: String?,
+    val gender: Int?,
+    val homepage: String?,
+    val id: Int?,
+    val imdb_id: String?,
+    val known_for_department: String?,
+    val name: String?,
+    val place_of_birth: String?,
+    val popularity: Double?,
+    val profile_path: String?,
+)
--- app/src/main/java/com/buzinasgeekbrains/themoviedb/view/ActorDetailsFragment.kt
+++ app/src/main/java/com/buzinasgeekbrains/themoviedb/view/ActorDetailsFragment.kt
@@ -6,11 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.buzinasgeekbrains.themoviedb.BuildConfig
 import com.buzinasgeekbrains.themoviedb.viewmodel.ActorDetailsViewModel
 import com.buzinasgeekbrains.themoviedb.databinding.ActorDetailsFragmentBinding
 import com.buzinasgeekbrains.themoviedb.model.Actor
+import com.buzinasgeekbrains.themoviedb.model.ActorDTO
+import com.buzinasgeekbrains.themoviedb.model.ActorLoader
 
 class ActorDetailsFragment : Fragment() {
 
@@ -30,7 +32,7 @@ class ActorDetailsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = ActorDetailsFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -39,12 +41,23 @@ class ActorDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.filmedInList.layoutManager = LinearLayoutManager(requireActivity())
         arguments?.getParcelable<Actor>(BUNDLE_EXTRA)?.let {
+
             binding.actorNameMain.text = it.name
-            binding.actorBiography.text = it.biography
-            binding.placeOfBirthList.text = it.placeOfBirth
-            binding.birthdayList.text = it.birthday
-            binding.genderList.text = if (it.gender == 1) "Male" else "Female"
-            binding.popularityList.text = it.popularity.toString()
+            ActorLoader.load(it, object: ActorLoader.OnActorLoadListener {
+                override fun onLoaded(actorDTO: ActorDTO) {
+                        binding.actorBiographyText.text = actorDTO.biography
+                        binding.placeOfBirthList.append(" ${actorDTO.place_of_birth}")
+                        binding.birthdayList.append(" ${actorDTO.birthday}")
+                        binding.genderList.append(" ${if (actorDTO.gender == 1) " Male" else " Female"}")
+                        binding.popularityList.append(" ${actorDTO.popularity.toString()}")
+
+                }
+
+                override fun onFailed(throwable: Throwable) {
+                    Toast.makeText(requireActivity(), throwable.message, Toast.LENGTH_SHORT).show()
+                }
+            })
+
         }
     }
 
