35bdb83aa2d5911105bbfc40e8ca1d3d3181f826
sashabuzin <n9159648714@gmail.com>
2022-06-18 17:17:50 +0300

    create deprecated CONNECTIVITY_ACTION broadcast receiver.

--- app/src/main/java/com/buzinasgeekbrains/themoviedb/view/MainActivity.kt
+++ app/src/main/java/com/buzinasgeekbrains/themoviedb/view/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.buzinasgeekbrains.themoviedb.view
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.buzinasgeekbrains.themoviedb.R
+import com.buzinasgeekbrains.themoviedb.broadcastReceiver.MainBroadcastReceiver
 import com.buzinasgeekbrains.themoviedb.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
         savedInstanceState ?: replaceMainContainer(MainFragment.newInstance())
 
         binding.bottomToolbar.toolbarMainBtn.setOnClickListener{
@@ -29,6 +35,11 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.container_main, fragment)
             .commit()
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 
 }
